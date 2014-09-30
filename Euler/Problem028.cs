fb3f92f8573f0c1543d08540ed448d195f5a91f2
andrewau <andrewau@microsoft.com>
2014-09-30 06:25:42 -0700

    Problem 028

---
+++ Euler/Problem028.cs
@@ -0,0 +1,20 @@
+﻿namespace Euler
+{
+    using System;
+
+    internal static partial class Program
+    {
+        public static void Problem028()
+        {
+            long sum = 1;
+            for (int i = 1; i < (1001 + 1) / 2; i++)
+            {
+                long end = (2 * i + 1) * (2 * i + 1);
+                long diff = 2 * i;
+                long start = end - 3 * diff;
+                sum += (start + end) * 4 / 2;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
